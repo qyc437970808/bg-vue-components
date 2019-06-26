0542936a321fca4df2d373b8bf5887b154b2fa9f
qyc <qinyongchao@banggood.com>
2019-06-26 19:19:24 +0800

    components for banggood

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import loading from './components/loading/loading'
 import loadMore from './components/loadMore/loadMore'
 
-module.exports={
+export default {
     install:function(Vue,options){
         Vue.component('bg-loading',loading);
         Vue.component('bg-loadMore',loadMore);
