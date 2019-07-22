9fe427f3d4de23d5c7796d6aac7511ec73c5123f
jiangminxi <jiangminxi@banggood.com>
2019-07-22 10:17:25 +0800

    webpack conf修改

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,16 +10,12 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
+const getComponentPath = require('./getComponentPath');
 // const env = require('../config/prod.env')
 const env = config.build[process.env.env_config+'Env']
 
 // 整理入口
-const components = require('../components.json')
-const entrys = {}
-Object.keys(components).forEach(item => {
-  entrys[item] = components[item]
-})
+const entrys = getComponentPath(path.resolve(__dirname, '../packages'));
 
 const webpackConfig = merge(baseWebpackConfig, {
   entry: entrys,
@@ -55,12 +51,12 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('theme/[name].css'),
+      filename: utils.assetsPath('theme/[name].css')
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
       // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      // allChunks: true,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
