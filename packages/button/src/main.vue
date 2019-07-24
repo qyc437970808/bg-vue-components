8bffdbf4896d54e2643f791a3d401d5c4fdbc72a
jiangminxi <jiangminxi@banggood.com>
2019-07-24 17:49:23 +0800

    添加小写文件

---
+++ packages/button/src/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-button">
+    我是一个Button组件
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BgButton',
+  methods: {
+    clickHandler() {
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.bg-button {
+  display: inline-block;
+}
+</style>
